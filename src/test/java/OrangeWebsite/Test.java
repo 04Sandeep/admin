cdd60cd55a676eb62bce4245c6e227003792bdc7
04Sandeep <110839065+04Sandeep@users.noreply.github.com>
2022-09-07 21:14:39 +0545

    Delete src directory

--- src/test/java/OrangeWebsite/Test.java
+++
@@ -1,8 +0,0 @@
-package OrangeWebsite;
-
-public class Test extends BaseClass {
-    public static void main(String[] args) throws InterruptedException {
-        setup();
-        pageFactory.getAdminPage().adminPage();
-    }
-}
